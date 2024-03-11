e1b3dccb3bccac34b84853c7f98c3bdc307c2dbc
bell014 <neder10tarroui@gmail.com>
2024-03-11 17:20:23 +0100

    removed age

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -5,8 +5,7 @@ const user = new mongoose.model('user', {
   email: { type: String,},
   password: { type: String,},
   username: { type: String,},
-  lastname: { type: String,},
-  age: { type: String, }
+  lastname: { type: String,}
 });
 
 module.exports = user ;
