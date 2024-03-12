18e02513c0b635329a23157a25389c737fe6b996
bell014 <neder10tarroui@gmail.com>
2024-03-12 12:54:45 +0100

    node v18.13

--- Backend/config/connect.js
+++ Backend/config/connect.js
@@ -1,7 +1,6 @@
 
 const mongoose = require('mongoose');
-
-mongoose.connect("mongodb://localhost:27017/test")
+mongoose.connect("mongodb://127.0.0.1:27017/test")
  .then(() => console.log('Connected to MongoDB'))
  .catch(err => console.error('Could not connect to MongoDB', err));
 
